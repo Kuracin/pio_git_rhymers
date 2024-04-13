989876f750e972ee427872dbc5674b2dbde1acf1
Milosz <245857@edu.p.lodz.pl>
2024-04-13 19:01:15 +0200

    4.2 poprawa nazw metod

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,10 +1,10 @@
 package edu.kis.vh.nursery;
 
-public class HanoiRhymer extends defaultCountingOutRhymer {
+public class HanoiRhymer extends DefaultRhymer {
 
     int totalRejected = 0;
 
-    public int reportRejected() {
+    public int printRejectedReports() {
         return totalRejected;
     }
 
