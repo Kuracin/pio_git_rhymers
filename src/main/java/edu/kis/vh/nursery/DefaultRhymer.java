e4ba60a4348f310702d286dcdf349c9d24192457
Natalia Balicka <245763@edu.p.lodz.pl>
2024-05-19 14:03:24 +0200

    poprawka

--- src/main/java/edu/kis/vh/nursery/DefaultRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultRhymer.java
@@ -2,8 +2,6 @@ package edu.kis.vh.nursery;
 
 public class DefaultRhymer {
 
-    public static final int minusOne = -1;
-    public static final int eleven = 11;
     private int[] NUMBERS = new int[12];
 
     public int total = -1;
@@ -13,23 +11,23 @@ public class DefaultRhymer {
             NUMBERS[++total] = in;
     }
 
-    public boolean callCheck() {
-            return total == minusOne;
+        public boolean callCheck() {
+            return total == -1;
         }
         
-    public boolean isFull() {
-        return total == eleven;
-    }
+            public boolean isFull() {
+                return total == 11;
+            }
         
                 protected int peekaboo() {
                     if (callCheck())
-                        return minusOne;
+                        return -1;
                     return NUMBERS[total];
                 }
             
                     public int countOut() {
                         if (callCheck())
-                            return minusOne;
+                            return -1;
                         return NUMBERS[total--];
                     }
 
