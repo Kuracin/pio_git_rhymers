a5d8a7c44f498c556c0fd181fd555c61ae720aa0
Milosz <245857@edu.p.lodz.pl>
2024-06-02 10:33:17 +0200

    13. Ekstrakcja zawartosci maina oraz zmiana nazw funkcji

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -1,5 +1,4 @@
 package edu.kis.vh.nursery;
-
 import edu.kis.vh.nursery.factory.DefaultRhymersFactory;
 import edu.kis.vh.nursery.factory.Rhymersfactory;
 
@@ -7,27 +6,36 @@ class RhymersDemo {
 
     public static void main(String[] args) {
         Rhymersfactory factory = new DefaultRhymersFactory();
-        
-        DefaultRhymer[] rhymers = { factory.GetStandardRhymer(), factory.GetFalseRhymer(),
-                factory.GetFIFORhymer(), factory.GetHanoiRhymer()};
-        
+
+        testRhymers(factory);
+    }
+
+    private static void testRhymers(Rhymersfactory factory) {
+        DefaultRhymer[] rhymers = { factory.getStandardRhymer(), factory.getFalseRhymer(),
+                factory.getFIFORhymer(), factory.getHanoiRhymer()};
+
+
         for (int i = 1; i < 15; i++)
             for (int j = 0; j < 3; j++)
                 rhymers[j].countIn(i);
-        
+
+
         java.util.Random rn = new java.util.Random();
         for (int i = 1; i < 15; i++)
             rhymers[3].countIn(rn.nextInt(20));
-        
+
+
         for (int i = 0; i < rhymers.length; i++) {
             while (!rhymers[i].callCheck())
                 System.out.print(rhymers[i].countOut() + "  ");
             System.out.println();
         }
-        
+
+
         System.out.println("total rejected is "
                 + ((HanoiRhymer) rhymers[3]).printRejectedReports());
-        
+
     }
-    
+
+
 }
