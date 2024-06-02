1e580b41d0f84641bbe4a1679f0703bf4c75ee34
Milosz <245857@edu.p.lodz.pl>
2024-06-02 11:10:08 +0200

    15. Testy przeprowadzone pomyslnie

--- src/test/java/edu/kis/vh/nursery/RhymersJUnitTest.java
+++ src/test/java/edu/kis/vh/nursery/RhymersJUnitTest.java
@@ -4,7 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class RhymersJUnitTest {
-
+    //tests completed successfully, process finished with exit code 0
     @Test
     public void testCountIn() {
         DefaultRhymer rhymer = new DefaultRhymer();
